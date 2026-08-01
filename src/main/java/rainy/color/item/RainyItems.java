5589b7222a9098b4229654ff471d1d609062a027
Abdulaleam <167490133+Abdulaleam@users.noreply.github.com>
2026-08-01 19:37:53 +0300

    added the start item, gonna make it work just like /start in a bit

--- src/main/java/rainy/color/item/RainyItems.java
+++ src/main/java/rainy/color/item/RainyItems.java
@@ -1,16 +1,24 @@
 package rainy.color.item;
 
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 import rainy.color.Color;
 
 public class RainyItems {
+    public static final Item START = registerItem("start", new Item(new Item.Settings()));
 
     private static Item registerItem(String name, Item item){
         return Registry.register(Registries.ITEM, Identifier.of(Color.MOD_ID, name), item);
     }
 
-    public static void registerRainyItems() {}
+    public static void registerRainyItems() {
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT).register(entries -> {
+            entries.add(START);
+        });
+    }
+
 }
